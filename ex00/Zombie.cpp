36d1ab83fe74df8517ef2b6904b7a3957fe751cb
dteruya <diegokteruya@gmail.com>
2025-12-17 17:37:21 -0300

    aval2

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,11 +6,21 @@
 /*   By: dteruya <dteruya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/14 11:38:54 by dteruya           #+#    #+#             */
-/*   Updated: 2025/12/14 11:38:55 by dteruya          ###   ########.fr       */
+/*   Updated: 2025/12/17 17:01:45 by dteruya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+Zombie::Zombie(void)
+{
+	std::cout << "Constructor called. \n";
+}
+
+Zombie::~Zombie(void)
+{
+	std::cout << "Destructor called. \n";
+}
 
 void Zombie::setName(std::string name)
 {
--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: dteruya <dteruya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/14 11:38:59 by dteruya           #+#    #+#             */
-/*   Updated: 2025/12/14 11:39:00 by dteruya          ###   ########.fr       */
+/*   Updated: 2025/12/17 16:52:15 by dteruya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ class Zombie
 		std::string name;
 
 	public:
+		Zombie(void);
+		~Zombie(void);
 		void announce(void);
 		void setName(std::string name);
 		std::string getName(void);
